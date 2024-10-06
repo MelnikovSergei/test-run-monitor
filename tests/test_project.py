3276162c760a1a606583e0d266c9af3d6b887d23
Helsing <melnikof2012@gmail.com>
2024-10-06 19:56:58 +0200

    fix

--- tests/test_project.py
+++ tests/test_project.py
@@ -9,6 +9,7 @@ from src.utils.project import Project
 class TestProject(unittest.TestCase):
     def setUp(self):
         self.project = Project()
+        self.custom_path_project = Project('custom')
 
     def test_get_project_path(self):
         self.assertIsNone(self.project.get_path())
@@ -17,7 +18,9 @@ class TestProject(unittest.TestCase):
         self.project.path = 'test'
         self.assertEqual(self.project.get_path(), 'test')
         
-        
+    def test_get_custom_project_path(self):
+        self.assertEqual(self.custom_path_project.get_path(), 'custom')
+               
 
 if __name__ == '__main__':
-    unittest.main(start_dir='src/tests')
+    unittest.main(start_dir='tests')
