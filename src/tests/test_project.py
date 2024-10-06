759756c0e6696ec0ce3bce6af09b4f113483b58d
Helsing <melnikof2012@gmail.com>
2024-10-06 19:15:03 +0200

    refactor

---
+++ src/tests/test_project.py
@@ -0,0 +1,19 @@
+import unittest
+from src.project import Project
+
+
+class TestProject(unittest.TestCase):
+    def setUp(self):
+        self.project = Project()
+
+    def test_get_project_path(self):
+        pass
+        
+       
+        
+        
+        
+        
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ src/utils/project.py
@@ -0,0 +1,30 @@
+
+
+class Project:
+    """
+    A class representing a project.
+    """
+
+    def __init__(self, path):
+        """
+        Initializes a new instance of the Project class.
+
+        Parameters
+        ----------
+        path : str
+            The path to the project directory.
+        """
+        
+        self.path = path
+        
+    def get_path(self):
+        """
+        Returns the path to the project directory.
+
+        Returns
+        -------
+        str
+            The path to the project directory.
+        """
+        
+        return self.path
