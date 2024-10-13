c64b8ca1a72a2aca870cca8afcbbbe7fb6720565
Helsing <melnikof2012@gmail.com>
2024-10-13 22:51:49 +0200

    update pages

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -7,7 +7,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import NoAlertPresentException
 from selenium.webdriver.support import expected_conditions as EC
-
+##
 
 def SHORT_WAIT(wait_time: int = 1):
     time.sleep(wait_time)
